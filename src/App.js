bbbf1f4f65363a6bf9be749f95d312c5b2f11a75
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-02 19:45:05 -0700

    fill out menu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
-import Menu from "./Menu";
-import Categories from "./Categories";
+import { Menu } from "./Menu";
+import { Categories } from "./Categories";
 import items from "./data";
 
 function App() {
+    const [menuItems, setMenuItems] = useState(items);
+    const [categories, setCategories] = useState([]);
     return (
-        <h2>menu project setup</h2>
+        <main>
+            <section className="menu section">
+                <div className="title">
+                    <h2>Our Menu</h2>
+                    <div className="underline"></div>
+                </div>
+                <Categories />
+                <Menu items={menuItems} />
+            </section>
+        </main>
     )
 }
 
--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-export const Menu = () => {
+export const Menu = ({ items }) => {
     return (
-        <div>
-            <h2>menu component</h2>
+        <div className="section-center">
+            {items.map((menuItem) => {
+                const { id, title, img, desc, price } = menuItem
+                return <article key={id} className="menu-item">
+                    <img src={img} alt={title} className="photo" />
+                    <div className="item-info">
+                        <header>
+                            <h4>
+                                {title}
+                            </h4>
+                            <h4 className="price">${price}</h4>
+                        </header>
+                        <div className="item-text">{desc}</div>
+                    </div>
+                </article>
+            })}
         </div>
     )
 }
